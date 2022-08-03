d95f4d5399b16f2f6616632289f4eba774c35a92
nemo-omen <nemo.omen@gmail.com>
2022-08-03 16:57:05 -0500

    Fail to seed again

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,36 +1,29 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClientPkg } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { process } from '@types/node';
+
+const PrismaClient = PrismaClientPkg.PrismaClient;
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const user1 = await prisma.user.upsert({
-    where: {
-      email: 'me@me.me',
-    },
-    update: {},
-    create: {
+  const user1 = await prisma.user.create({
+    data: {
       email: 'me@me.me',
       username: 'me',
       passwordHash: await bcrypt.hash('123456789', 10)
     }
   });
 
-  const user2 = await prisma.user.upsert({
-    where: {
-      email: 'you@you.you',
-    },
-    update: {},
-    create: {
+  const user2 = await prisma.user.create({
+    data: {
       email: 'you@you.you',
       username: 'you',
       passwordHash: await bcrypt.hash('123456789', 10)
     }
   });
+  console.log({ user1, user2 });
 
-  console.log({ user1 });
-  console.log({ user2 });
 
   // const character1 = await prisma.character.upsert({
   //   where: {
