7a10461f9829690d339f423d3dec1d09e33d75ab
nemo-omen <nemo.omen@gmail.com>
2022-08-11 11:47:00 -0500

    Still not sure what I'm doing

--- core/src/Item.ts
+++ core/src/Item.ts
@@ -1,3 +1,17 @@
+import { Actor } from './Actor';
+import { Attack } from './capabilities/Attack';
 export class Item {
+  minDamage: number;
+  maxDamage: number;
+  attack: Attack;
 
+  constructor (minDamage: number, maxDamage: number) {
+    this.minDamage = minDamage;
+    this.maxDamage = maxDamage;
+    this.attack = new Attack(minDamage, maxDamage);
+  }
+
+  hit(target: Actor) {
+    this.attack.hit(target);
+  }
 }
